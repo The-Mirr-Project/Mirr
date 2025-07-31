56040a8a7931c85e690e5df64ee8ef2744c394ad
Shrey719 <shrey150436@gmail.com>
2025-07-31 15:00:33 -0400

    cookery

--- src/client.js
+++ src/client.js
@@ -2,6 +2,8 @@ import { $mirr$fetch } from "./client/fetch.js";
 import { $mirr$location } from "./client/location.js";
 import { $mirr$open } from "./client/open.js";
 import { $mirr$navigator } from "./client/navigator.js";
+import { $mirr$history } from './client/history.js'
+
 import { initMutObs } from "./client/mutobs.js";
 
 Object.assign(globalThis, {
@@ -9,6 +11,7 @@ Object.assign(globalThis, {
   $mirr$location,
   $mirr$open,
   $mirr$navigator,
+  $mirr$history
 });
 
 initMutObs();
--- src/rewrite/js.js
+++ src/rewrite/js.js
@@ -2,60 +2,63 @@ import { parseScript } from "meriyah";
 import { walk } from "estree-walker";
 import { generate } from "astring";
 
-function rewriteJavascript(jsinput) {
-  const ast = parseScript(jsinput);
+const pairs = {
+  fetch: "$mirr$fetch",
+  location: "$mirr$location",
+  open: "$mirr$open",
+  navigator: "$mirr$navigator",
+  history: "$mirr$history"
+}
+
+function rewriteJavascript(code) {
+  const ast = parseScript(code, { next: true, loc: false });
 
   walk(ast, {
-    enter(node) {
-      // fetch -> $mirr$fetch
-      if (
-        node.type === "CallExpression" &&
-        node.callee.type === "Identifier" &&
-        node.callee.name === "fetch"
-      ) {
-        node.callee.name = "$mirr$fetch";
-        return;
+    enter(node, parent) {
+      // plain identifiers (fetch - $mirr$fetch, location - mirr$locaion, etc)
+      if (node.type == "Identifier") {
+        const name = node.name;
+        if (!Object.hasOwn(pairs, name)) return;
+
+        // dont touch declarations
+        if (
+          (parent?.type == "VariableDeclarator" && parent.id == node) ||
+          (parent?.type == "FunctionDeclaration" && parent.id == node) ||
+          (parent?.type == "FunctionExpression" && parent.id == node) ||
+          (parent?.type == "ClassDeclaration" && parent.id == node) ||
+          (parent?.type == "ClassExpression" && parent.id == node) ||
+          (parent?.type == "Property" && parent.key == node && !parent.computed) ||
+          (parent?.type == "ImportSpecifier") ||
+          (parent?.type == "ObjectPattern") ||
+          (parent?.type == "Property" && parent.parent?.type == "ObjectPattern")
+        ) {
+          return;
+        }
+
+        node.name = pairs[name];
       }
 
-      // window.* things
+      // global identifiers for now are window globalthis and top. im probably missing like 4 lol
       if (
-        node.type === "MemberExpression" &&
+        node.type == "MemberExpression" &&
         !node.computed &&
-        node.object.type === "Identifier" &&
-        (node.object.name === "window" ||
-          node.object.name == "top" ||
-          node.object.name == "globalThis") &&
-        node.property.type === "Identifier"
+        node.object.type == "Identifier" &&
+        ["window", "globalThis", "top"].includes(node.object.name) &&
+        node.property.type == "Identifier"
       ) {
-        if (node.property.name === "location") {
-          this.replace({
-            type: "Identifier",
-            name: "$mirr$location",
-          });
-          return;
-        }
-        if (node.property.name === "open") {
-          this.replace({
-            type: "Identifier",
-            name: "$mirr$open",
-          });
-          return;
+        const name = node.property.name;
+        if (Object.hasOwn(pairs, name)) {
+          node.type = "Identifier";       
+          node.name = pairs[name];
+          delete node.object;
+          delete node.property;
         }
       }
-      // location -> $mirr$location
-      if (node.type === "Identifier" && node.name === "location") {
-        node.name = "$mirr$location";
-        return;
-      }
-      // navigator  -> $mirr$navigator
-      if (node.type === "Identifier" && node.name === "navigator") {
-        node.name = "$mirr$navigator";
-        return;
-      }
-    },
+    }
   });
 
   return generate(ast);
 }
 
-export default rewriteJavascript;
+
+export default rewriteJavascript
